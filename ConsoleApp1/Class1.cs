6b8f8f9196f28dc766eb1dd294c78dd957561f5a
wojci <wojciechkajor@gmail.com>
2025-07-13 19:10:57 +0200

    Add VertexFactory Class

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class VertexFactory
+    {
+        public static List<Vertex> StworzListeCoord()
+        {
+            var lista = new List<Vertex>
+        {
+            new Vertex(1.0, 2.0, 3.0),
+            new Vertex(4.0, 5.0, 6.0),
+            new Vertex(7.0, 8.0, 9.0)
+        };
+
+            return lista;
+        }
+    }
+}
